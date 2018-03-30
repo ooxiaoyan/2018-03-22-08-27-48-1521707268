974e66cf43e6ed30f7b57df5186bbf6807129560
ooxiaoyan <810978527@qq.com>
2018-03-31 01:53:26 +0800

    second commit section3 PracticeB.java

--- src/main/java/cn/school/thoughtworks/section3/PracticeB.java
+++ src/main/java/cn/school/thoughtworks/section3/PracticeB.java
@@ -5,7 +5,6 @@ import java.util.Map;
 
 public class PracticeB {
     Map<String,Integer> createUpdatedCollection(Map<String,Integer> collectionA, Map<String,List<String>> object) {
-        //实现练习要求，并改写该行代码。
         List<String> list2 = object.get("value");
         for (String s : list2) {
             for (Map.Entry<String, Integer> entry : collectionA.entrySet()) {
