d55dfceb09d8426de609321f7e3bb1c37f61c502
ooxiaoyan <810978527@qq.com>
2018-03-30 22:52:26 +0800

    commit section1 PracticeB.java

--- src/main/java/cn/school/thoughtworks/section1/PracticeB.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeB.java
@@ -1,10 +1,20 @@
 package cn.school.thoughtworks.section1;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class PracticeB {
     List<String> collectSameElements(List<String> collection1, List<List<String>> collection2) {
-        //实现练习要求，并改写该行代码。
-        return null;
+        List<String> distinct = collection1;
+        for (List<String> list : collection2) {
+            distinct = distinct.stream()
+                .filter(item -> !list.contains(item)).collect(Collectors.toList());
+        }
+        List<String> finalDistinct = distinct;
+        List<String> result = collection1.stream()
+                .filter(item -> !finalDistinct.contains(item)).collect(Collectors.toList());
+        return result;
     }
 }
