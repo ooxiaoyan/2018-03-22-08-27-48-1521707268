515a75f0380fb4b047d5fdc0e2bb97a17c4b41be
ooxiaoyan <810978527@qq.com>
2018-03-31 02:18:16 +0800

    commit section3 PracticeD.java

--- src/main/java/cn/school/thoughtworks/section3/PracticeD.java
+++ src/main/java/cn/school/thoughtworks/section3/PracticeD.java
@@ -1,12 +1,16 @@
 package cn.school.thoughtworks.section3;
 
+import cn.school.thoughtworks.section2.PracticeB;
+
 import java.util.List;
 import java.util.Map;
 
 public class PracticeD {
     Map<String,Integer> createUpdatedCollection(List<String> collectionA, Map<String,List<String>> object) {
-        //实现练习要求，并改写该行代码。
-
-        return null;
+        cn.school.thoughtworks.section2.PracticeB practiceB2 = new PracticeB();
+        Map<String, Integer> collection3 = practiceB2.countSameElements(collectionA);
+        cn.school.thoughtworks.section3.PracticeB practiceB3 = new cn.school.thoughtworks.section3.PracticeB();
+        Map<String,Integer> result = practiceB3.createUpdatedCollection(collection3, object);
+        return result;
     }
 }
